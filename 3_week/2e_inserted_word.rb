fe4a664606e86b66751a78278fa493deea210cd7
kangkyu <kangkyu1111@gmail.com>
2016-01-03 21:30:54 -0800

    3w/2e count 'word' in the subject file when replaced give different message when nothing changed use fileutils.mv instead of commandline mv command
    modified:   3_week/2e_inserted_word.rb

--- 3_week/2e_inserted_word.rb
+++ 3_week/2e_inserted_word.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 puts 'Filename?'
 filename = gets.chomp
 
@@ -6,20 +8,27 @@ unless File.file?(filename)
   exit
 end
 
+word_count = 0
+
 File.open(filename, 'r') do |file|
   File.open("#{filename}_new", 'w') do |new_file|
     file.readlines.each do |line|
       words = line.split(' ')
       if words.include?('word')
+        word_count += words.count {|word| word == 'word'}
         words.map!{|word| word == 'word' ? 'inserted word' : word }
       end
       new_file.puts words.join(' ')
     end
   end
-  puts "'word' replaced to 'inserted word' in file '#{filename}'"
 end
-`mv #{filename}_new #{filename}`
+FileUtils.mv "#{filename}_new", filename # use fileutils stdlib, not direct command
 
+case word_count
+when 0 then puts "no 'word' to be replaced in file '#{filename}'"
+else
+  puts "'word' replaced to 'inserted word' in file '#{filename}' (#{word_count} count)"
+end
 
 # ruby 2e_inserted_word.rb
 # 'texts' as filename
