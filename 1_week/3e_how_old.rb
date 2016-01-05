604bbc666ffb21b66498e6a59dfa9c59a3ee93e3
kangkyu <kangkyu1111@gmail.com>
2016-01-05 10:26:56 -0800

    3w/8e decimal_years_old method returns decimal

--- 1_week/3e_how_old.rb
+++ 1_week/3e_how_old.rb
@@ -1,7 +1,6 @@
 def decimal_years_old(seconds_old)
   year_in_seconds = 365 * 24 * 60 * 60
-  format "%.2f", seconds_old.to_f / year_in_seconds
+  seconds_old.to_f / year_in_seconds
 end
-seconds_old = 979000000
 
-p decimal_years_old(seconds_old)
+p format "%.2f", decimal_years_old(979000000)
